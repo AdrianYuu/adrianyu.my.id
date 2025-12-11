3784658bd478d097786fa08995aac816487c0f8e
Adrian Yu <adrian.yu001@gmail.com>
2025-12-11 19:13:23 +0700

    feat: update projects

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -32,29 +32,76 @@ export const PROJECTS: IProject[] = [
     {
         title: "Atlantis",
         descriptions: [
-            "Atlantis is a website used by laboratory assistants."
+            "Website used by Software Laboratory Center staff and laboratory coordinators from other laboratories at BINUS University, serving over 200 users.",
+            "Supports Software Laboratory Center promotion registration.",
+            "Provides a voting system for selecting the Best Assistant.",
+            "Includes component mapping features for publishing academic scores to BINUSMAYA.",
+            "Allows registration and management of course information.",
+            "Offers a file downloader with data retrieved directly from the internal database.",
+            "Displays student schedules for quick reference.",
+            "Displays lecturer schedules for coordination and planning.",
         ],
         type: ProjectType.PROFESSIONAL,
-        technologies: ["React", "shadcn/ui", "TypeScript", "TanStack Query", ".NET Core", "EF Core", "Microsoft SQL Server", "IIS"],
-        photos: [AtlantisLandingPage]
+        technologies: [
+            "React",
+            "TypeScript",
+            "Tailwind",
+            "Shadcn",
+            "TanStack Query",
+            ".NET Core",
+            "EF Core",
+            "Microsoft SQL Server",
+            "IIS"
+        ],
+        photos: [
+            AtlantisLandingPage
+        ]
     },
     {
         title: "Attendance Management",
         descriptions: [
-            "Attendance Management is an attendance website."
+            "Web application for managing internship student attendance at the Enrichment School of Computer Science, BINUS University, used daily by over 120 internship students.",
+            "Simplifies supervisors’ workflow by providing automated attendance recaps that can be exported directly to Excel.",
+            "Implemented anti-cheating mechanisms by validating student location during check-in.",
+            "Added a dynamic attendance code that refreshes every 10 seconds and is only visible to classroom devices or authorized admin users."
         ],
         type: ProjectType.PROFESSIONAL,
-        technologies: ["React", "Tailwind", "Express", "TypeScript", "Prisma", "MySQL", "Docker"],
-        photos: [AttendanceManagementLoginPage, AttendanceManagementStudentHomePage, AttendanceManagementClassroomHomePage, AttendanceManagementAdminHomePage]
+        technologies: [
+            "React",
+            "TypeScript",
+            "Tailwind",
+            "Express",
+            "Prisma",
+            "MySQL",
+            "Docker"
+        ],
+        photos: [
+            AttendanceManagementLoginPage,
+            AttendanceManagementStudentHomePage,
+            AttendanceManagementClassroomHomePage,
+            AttendanceManagementAdminHomePage
+        ]
     },
     {
         title: "ERamen",
         descriptions: [
-            "ERamen is a ramen website."
+            "Website about a ramen shop. Showing its products and store information.",
+            "There are five pages including home, products, promotion, about us, and contact us.",
+            "Made as my submission for my Human and Computer Interaction course laboratory project."
         ],
         type: ProjectType.PERSONAL,
-        technologies: ["HTML", "CSS", "JavaScript"],
-        photos: [ERamenHome, ERamenProduct, ERamenPromotion, ERamenAboutUs, ERamenContactUs],
+        technologies: [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        photos: [
+            ERamenHome,
+            ERamenProduct,
+            ERamenPromotion,
+            ERamenAboutUs,
+            ERamenContactUs
+        ],
         githubs: [
             {
                 title: "eramen",
